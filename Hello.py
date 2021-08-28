acd3667874250409692a372d9087450b51edb3e8
Jean Andrade <74221819+jeanfma@users.noreply.github.com>
2021-08-28 17:52:26 -0300

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,81 @@
+print("Olá Mundo!")
+#usar(#) para fazer comentarios
+print("Tudo bem ?")
+""" comentarios
+	aqui
+	tbm
+"""
+print(2+2)
+print(2**2)
+print(10 % 3)
+print(12-2*1)
+print(4*2)
+
+#atribuir um variavel
+
+minha_variavel = "Olá mundo!"
+print (minha_variavel)
+
+var1 = 1 #variavel inteira
+var2 = 1.1 #variavel float ou real
+var3 = "Eu sou uma string" #variavel string/char
+var4 = True # variavel booleana
+var5 = False # variavel booleana
+print (var3)
+"""
+x = 3 
+y = 3 
+z = 4
+# Operadores lógicos  and/ or 
+#print (x == y == z)
+print (x == y or x == z and z == y)
+"""
+#codicionais   if
+"""
+x = 1 
+y = 100000000
+
+if x > y:
+	print("x é maior que y")
+if y > x:	
+
+	print("y é maior que x")
+
+	#tecla tab indentação = 4 espaços
+"""
+#comando executado else caso o if não rode
+""""
+x = 1 
+y = 2
+""
+if x > y:
+	print("x maior que y")
+else:
+	print("x não é maior que y")
+"""
+# Sintaxe = organização do codigo
+# comando elif caso mais que if / else
+"""
+x = 1 
+
+while x < 10:
+	print(x)
+	x += 1 # x = x + 1
+"""
+"""
+lista1 = [1,2,3,4,5]
+lista2 = ["Ola","mundo","!"]
+lista3 = [0,"hello","biscoito","bolacha",9.99,True]
+
+for i in lista3:
+	print(i)
+"""
+for i in range(10,20,2):
+	print(i)
+
+numero = input("Digite um numero: ")
+print("O numero digitado eh: ")
+print(numero)
+
+#concatenação união de duas strings
+# cmd comandos dir cd ou cd.. voltar
